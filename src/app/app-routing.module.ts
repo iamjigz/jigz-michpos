561dcd19e660e2fb290eec2f29fcf6707cc966b9
iamjigz <jigzimon@gmail.com>
2019-03-19 16:23:20 +0800

    Added inventory route and flex-layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './routes/admin/dashboard/dashboard.component';
+import { InventoryComponent } from './routes/inventory/inventory.component';
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent
+  },
+  {
+    path: 'inventory',
+    component: InventoryComponent
   }
 ];
 

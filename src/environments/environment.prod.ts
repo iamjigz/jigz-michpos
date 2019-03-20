b099b801fae8896af514d6df33dd947e8ba6f377
iamjigz <jigzimon@gmail.com>
2019-03-21 01:12:34 +0800

    Updated prod env values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB7uLCMW1oiSTYEdhTM9WMODs8ZHz6R9B0',
+    authDomain: 'michpos-6ad8b.firebaseapp.com',
+    // TODO: Change databaseURL for prod?
+    databaseURL: 'https://michpos-6ad8b.firebaseio.com',
+    projectId: 'michpos-6ad8b',
+    storageBucket: 'michpos-6ad8b.appspot.com',
+    messagingSenderId: '958177901320'
+  }
 };

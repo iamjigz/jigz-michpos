47df53154a3f56b5e83f8e75c18f76367afee33b
iamjigz <jigzimon@gmail.com>
2019-03-20 23:21:54 +0800

    Add animated login form

--- src/app/routes/login/login.component.ts
+++ src/app/routes/login/login.component.ts
@@ -1,14 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
+
+import '../../../assets/login-animation.js';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-  constructor() {}
+export class LoginComponent implements AfterViewInit {
+  public loginGroup: FormGroup;
+  public email: string;
+  public password: string;
 
-  ngOnInit() {}
+  constructor(
+    private auth: AuthService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {}
 
-  login() {}
+  ngAfterViewInit() {
+    (window as any).initialize();
+
+    this.loginGroup = this.fb.group({
+      email: [this.email, Validators.required],
+      password: [this.password, Validators.required]
+    });
+  }
+
+  login() {
+    // console.log(`email: ${this.email} password: ${this.password}`);
+    // alert(`Email: ${this.email} Password: ${this.password}`);
+    this.auth
+      .login(this.email, this.password)
+      .then(res => {
+        console.log('You\'ve logged in', res);
+        this.router.navigate(['/']);
+      })
+      .catch(err => console.log(err));
+  }
 }
